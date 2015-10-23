e02224e3fb3694c67026ac4bafc46050737fed9a
ZenOokami <zaneblalock@gmail.com>
2015-10-23 03:00:17 -0400

    Update 1.7.12
    Adding some user-proofing, adding some console statements to discuss
    what's happening.

--- MassExtensionChanger.py
+++ MassExtensionChanger.py
@@ -1,20 +1,24 @@
 # Mass Extension Changer
 # Coded by Zane ZenOokami Blalock
-# Essence Of Zen
+# http://EssenceOfZen.org/
 
 # This program essentially takes in your entered directory and changes any specified extension
 # with another extension specified.
 
-# To-Do =========
-# *Add Menu
-# *Add more user-proofing
-# *Create a GUI?
+# Feel free to fork this project and make what you will with it, we simply ask that you give credit, and link back
+# to our source location at our site (EssenceOfZen.org) and our GitHub. Thank you <3
+
+# TODO: Add Menu
+# TODO: Add more user-proofing
+# TODO: Create a GUI?
+# TODO: Create more console communication to know what's going on
 
 
 import os
+import EssencePython
 
 # Global Variables ===========================
-VERSION = "1.3.1" # A variable to call for showing update once menu is made
+VERSION = "1.7.12" # A variable to call for showing update once menu is made
 
 DIRECTORY = "" # used for the directory url location
 DIRECTORY_ITEMS = [] # This is an array to house the items inside the directory
@@ -25,6 +29,7 @@ def changeDirectory(): # Function allows to change the Directory
 
     # We ask for the directory
     directory = "" # Create an empty string for the directory location
+    print("(You can copy the url from window's explorer directory bar)")
     user_input = input("Please enter directory location: ") # Have the user input the directory - they can copy and paste from window's explorer
     for item in user_input: # If the directory uses backslash (like from window's syntax), change them to forward slash for Python's syntax
         if item == '\\':
@@ -34,8 +39,12 @@ def changeDirectory(): # Function allows to change the Directory
     length = len(directory) # We grab the length of the directory, and use -1 to get the index of the last character
     if directory[length-1] != "/": # If there isn't a / at the end of the directory, add one.
         directory += "/"
+        print("Added '/' to the url!")
+        print("")
 
     DIRECTORY = directory # We set the global variable
+    print("Directory Changed!")
+    print("")
 
 
 def printDirectoryItems(directory): # Prints the items found in the Directory **REDO THIS CODE LATER SINCE UPDATE
@@ -51,31 +60,37 @@ def changeExtension(selection, target):
     global DIRECTORY
     global DIRECTORY_ITEMS
 
-
     for file in DIRECTORY_ITEMS:
         current_file = os.path.join(DIRECTORY, file) # We add the folder's url to the file name to create file's url
         if selection in current_file:
             print(current_file)
             new_name = current_file.replace(selection, target)
             output = os.rename(current_file, new_name)
+        else:
+            print("Selection was not found...")
+            print("")
 
-def updateDirectory():
+def updateDirectory(): # Updates the Directory with any changes that you've made on the files.
     global DIRECTORY
     global DIRECTORY_ITEMS
 
     DIRECTORY_ITEMS = os.listdir(DIRECTORY)
 
-# Main Function
+def menu(): # TODO: Finish this function
+    pass
+
+# Main Function ====================================================================================================
 def main():
+    EssencePython.EoZ_Logo()
+
     global DIRECTORY
     global DIRECTORY_ITEMS
     # Start the program by getting the directory
     # Later create a menu
 
-    switch = 1
-    while switch == 1:
+    main_switch = 1
+    while main_switch == 1:
         changeDirectory()
-        # directory_items = os.listdir(DIRECTORY)
         print()
 
         printDirectoryItems(DIRECTORY)
@@ -87,23 +102,23 @@ def main():
         print()
         print("Please enter what extension you wanted to switch it to")
         target = input("please enter extension different than before: ")
-
         print()
+
         changeExtension(selection, target)
         print()
         updateDirectory()
 
         answer = input("Are you finished?: ").lower()
-        loop = 1
-        while loop == 1:
+        continue_loop = 1
+        while continue_loop == 1:
             if answer == "yes" or answer == "y":
-                loop = 0
-                switch = 0
+                continue_loop = 0
+                main_switch = 0
             elif answer == "no" or answer == "n":
-                loop = 0
-                switch = 1
+                continue_loop = 0
+                main_switch = 1
             else:
                 print("Answer is invalid, try again please.")
-                loop = 1
+                continue_loop = 1
 
 main()
